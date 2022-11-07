14d72568602d0a1e7ba4cb4582dc97486e1bf953
laulaurose <laurarose@sund.ku.dk>
2022-11-07 14:35:21 +0100

    lauras første forsøg på env

---
+++ test-env/project/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  7 13:47:03 2022
+
+@author: qgf169
+"""
+
+import numpy as np
+from PIL import Image
+
+print("hello world")
+print(2+2)
+
+im = Image.open("luca.jpeg")
+print("image loaded")
