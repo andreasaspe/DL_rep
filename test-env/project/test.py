208e5f6066c50903bdfb1c39eace5e3814ee8338
laulaurose <laurarose@sund.ku.dk>
2022-11-07 15:54:49 +0100

    added packages - missing cuda and cudamat

--- test-env/project/test.py
+++ test-env/project/test.py
@@ -5,9 +5,19 @@ Created on Mon Nov  7 13:47:03 2022
 
 @author: qgf169
 """
-
+#mangler cudamat
 import numpy as np
+import _pickle as cPickle
+import matplotlib.pyplot as plt
+import shutil
+import scipy.io
+import configparser
+import argparse
+import sklearn
+import pickle
 from PIL import Image
+
+
 
 print("hello world")
 print(2+2)
